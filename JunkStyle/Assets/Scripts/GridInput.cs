68f2e9d2e2ad88b816be2fc04fc6481832ce358c
lin <ilnur_bakiev@inbox.ru>
2024-06-29 16:44:56 +0300

    Added Cinemachine Virtual Camera

--- JunkStyle/Assets/Scripts/GridInput.cs
+++ JunkStyle/Assets/Scripts/GridInput.cs
@@ -21,7 +21,15 @@ public class GridInput : MonoBehaviour
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
         if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            Cursor.visible = false;
             lastPosition = hit.point;
+        }
+        else
+        {
+            Cursor.visible = true;
+            lastPosition = Vector3.down;
+        }
 
         return lastPosition;
     }
