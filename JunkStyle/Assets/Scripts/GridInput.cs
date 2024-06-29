bd65b23df5f0a1f454ee5ba1bbf24828e082c303
lin <ilnur_bakiev@inbox.ru>
2024-06-29 03:41:36 +0300

    Created basic grid placement

---
+++ JunkStyle/Assets/Scripts/GridInput.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class GridInput : MonoBehaviour
+{
+    [SerializeField]
+    private Grid grid;
+
+    [SerializeField]
+    private GameObject cellIndicator;
+
+    private Vector3 lastPosition;
+
+    private void Update()
+    {
+        Vector3Int gridPosition = grid.WorldToCell(MousePosition());
+        cellIndicator.transform.position = grid.CellToWorld(gridPosition);
+    }
+
+    private Vector3 MousePosition()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out RaycastHit hit))
+            lastPosition = hit.point;
+
+        return lastPosition;
+    }
+}
