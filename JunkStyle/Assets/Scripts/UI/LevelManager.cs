133a1028bae9d196c8e3de733c18d59637f3b336
bakiLin <ilnur_bakiev@inbox.ru>
2025-08-24 16:53:06 +0300

    update

--- JunkStyle/Assets/Scripts/UI/LevelManager.cs
+++ JunkStyle/Assets/Scripts/UI/LevelManager.cs
@@ -2,14 +2,27 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using Zenject;
 
 public class LevelManager : MonoBehaviour
 {
+    [Inject]
+    private AudioManager audioManager;
+
+    [Inject]
+    private CursorManager cursorManager;
+
     [SerializeField]
     private Image blackoutImage;
 
     private void Start()
     {
+        if (SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            audioManager.Stop("theme");
+            cursorManager.Unlock();
+        }
+
         blackoutImage.DOFade(0f, 1f).SetUpdate(true);
     }
 
